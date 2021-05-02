067f26ffd849cc57254a0e3cedc001de60c62106
sambld <SAMIbld2001@#>
2021-05-02 20:58:47 +0100

    configure sanctum

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dotenv\Exception\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        $credentials =  request()->validate([
+            'username' => ['required' , 'min:4'],
+            'password' => ['required' , 'min:8']
+        ]);
+
+        if ( Auth::attempt($credentials)){
+            return response()->json([
+                Auth::user()
+            ], 200);
+        }
+
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'UserNotFound' => 'user not found'
+        ]);
+
+    }
+
+    public function logout()
+    {
+        if (Auth::check()){
+            Auth::logout();
+        }
+    }
+}
