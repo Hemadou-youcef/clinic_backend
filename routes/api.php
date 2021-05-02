638d5e4823808bfae926aab975be454578c92eb8
sambld <SAMIbld2001@#>
2021-05-02 20:58:47 +0100

    configure sanctum

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::middleware('auth:sanctum')->post('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/login' , [\App\Http\Controllers\LoginController::class , 'index']  );
+Route::post('/logout' , [\App\Http\Controllers\LoginController::class , 'logout']  );
+
+Route::get('/password' , function (){
+
+    return \Illuminate\Support\Facades\Hash::make('samisamo');
+});
+
+
